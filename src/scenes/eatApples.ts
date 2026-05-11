82c5bc15f5b26830a88a0580403757a491fc70e5
Ryan Brue <ryanbrue.dev@gmail.com>
2026-05-10 20:12:26 -0500

    FIX EAT APPLES

--- src/scenes/eatApples.ts
+++ src/scenes/eatApples.ts
@@ -1,9 +1,18 @@
-import { registerQuest } from "../../quests.js";
+import { Quest } from "../quests/quest.js";
+import type { QuestRuntime } from "../quests/quest.js";
 
-registerQuest({
-    id: "eat-5-apples",
-    label: "Novice Eater",
-    description: "Eat 5 apples",
-    isCompleted: (s) => s.flags["applesEaten"] >= 5,
-    onReward: (s) => s.addScore(10),
-});
+class EatApplesQuest extends Quest {
+  constructor() {
+    super("eat-5-apples", "Novice Eater", "Eat 5 apples");
+  }
+
+  override isCompleted(runtime: QuestRuntime): boolean {
+    return this.progressSinceAccept(runtime, "applesEaten") >= 5;
+  }
+
+  override onReward(runtime: QuestRuntime): void {
+    runtime.addScore(10);
+  }
+}
+
+export default new EatApplesQuest();
