ce4802948dd7f6ed33e30117af22c1896bfc0059
ryanabx <ryanabx@users.noreply.github.com>
2026-05-14 16:30:30 +0000

    deploy: fe6a3868e98d21a966773cd11595a3bde866169d

--- assets/wrapWall-CkGlgWfr.js
+++
@@ -1,1 +0,0 @@
-import{F as r}from"./feature-CPUWW1YZ.js";class a extends r{constructor(){super("wrapWall","Wrap at edges")}onRegister(e){e.setTeleport(!0)}}const l=new a;export{l as default};
---
+++ assets/wrapWall-Dqe3u1Hw.js
@@ -0,0 +1,1 @@
+import{F as r}from"./feature-2uzRfLcN.js";class a extends r{constructor(){super("wrapWall","Wrap at edges")}onRegister(e){e.setTeleport(!0)}}const l=new a;export{l as default};
