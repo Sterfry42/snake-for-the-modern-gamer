474633d20aab1226aeb53fd1bd16f4e0c7c96359
Ryan Brue <ryanbrue.dev@gmail.com>
2026-07-19 13:45:15 -0500

    My AI reported 60% less code but this looks like more code to me.

--- src/quests/definitions/reachLength.ts
+++ src/quests/definitions/reachLength.ts
@@ -1,18 +1,9 @@
-import { Quest } from '../quest.js';
-import type { QuestRuntime } from '../quest.js';
+import { LengthQuest } from './countingQuest.js';
 
-class ReachLengthQuest extends Quest {
-  constructor() {
-    super('reach-length-10', 'Stretch Goal', 'Grow your snake to length 10');
-  }
-
-  override isCompleted(runtime: QuestRuntime): boolean {
-    return runtime.getSnakeLength() >= 10;
-  }
-
-  override onReward(runtime: QuestRuntime): void {
-    runtime.addScore(15);
-  }
-}
-
-export default new ReachLengthQuest();
+export default new LengthQuest(
+  'reach-length-10',
+  'Stretch Goal',
+  'Grow your snake to length 10',
+  10,
+  15,
+);
--- src/quests/definitions/score50.ts
+++ src/quests/definitions/score50.ts
@@ -1,19 +1,10 @@
-import { Quest } from '../quest.js';
-import type { QuestRuntime } from '../quest.js';
+import { ScoreQuest } from './countingQuest.js';
 
-class ScoreFiftyQuest extends Quest {
-  constructor() {
-    super('score-50', 'Rookie Points', 'Reach a score of 50');
-  }
-
-  override isCompleted(runtime: QuestRuntime): boolean {
-    return runtime.getScore() >= 50;
-  }
-
-  override onReward(runtime: QuestRuntime): void {
-    runtime.addScore(15);
-    runtime.addCosmeticReward('style', 'charcoal-silk');
-  }
-}
-
-export default new ScoreFiftyQuest();
+export default new ScoreQuest(
+  'score-50',
+  'Rookie Points',
+  'Reach a score of 50',
+  50,
+  15,
+  { type: 'style', id: 'charcoal-silk' },
+);
